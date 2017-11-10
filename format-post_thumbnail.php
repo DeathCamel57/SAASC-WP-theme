0b7707aee26e9f09dfda9e24f34a6409c1e60684
DeathCamel57 <DeathCamel57@gmail.com>
2017-11-09 19:14:04 -0500

    Inital Image Post Support

--- format-post_thumbnail.php
+++ format-post_thumbnail.php
@@ -9,21 +9,16 @@
 <!-- Grab the featured image -->
 <?php if ( '' != get_the_post_thumbnail() ) { ?>
 
+    <?php
 
-	<!-- The anchor tag that will hold the post thumbnail -->
-	<a class="featured-image" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+        //The settings to be pased to the post_thumbnail
+        $args = array(
+            'class' => 'img-fluid' /*,
+            'alt'   => trim( strip_tags( $wp_postmeta->_wp_attachment_image_alt ) )*/
+        );
 
-		<?php
+        the_post_thumbnail( 'large-image', $args );
 
-			//The settings to be pased to the post_thumbnail
-			$args = array(
-				'class' => '',
-				'alt'   => trim( strip_tags( $wp_postmeta->_wp_attachment_image_alt ) )
-			);
-
-		 	the_post_thumbnail( 'large-image', $args );
-
-		  ?>
-	</a>
+      ?>
 
 <?php } ?>
